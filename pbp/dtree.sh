03d7d305e6d25cad2aa8560560d36018e97fc86c
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-01-23 15:23:07 -0500

    dtree.sh regenerates .json

--- pbp/dtree.sh
+++ pbp/dtree.sh
@@ -2,11 +2,15 @@
 # $1 is the working directory of the project (usually '.')
 # $2 is the directory of the pbp toolset (usually './pbp')
 # $3 is the name (sans suffix) of the .drawio file input and the name of the .frish output and the .py output
+rm -f out.*
+rm -f *.json
 set -e
 wd="$(realpath "$1")"
 pbpd="$(realpath "$2")"
 name="$3"
 cd "${pbpd}/dtree"
+node ${pbpd}/das/das2json.mjs dtree-transmogrifier.drawio
+./check-for-span-error.bash  dtree-transmogrifier.drawio.json
 python main.py "${pbpd}/" "$3.drawio" main  dtree-transmogrifier.drawio.json | node ${pbpd}/kernel/splitoutput.js
 if [ -f out.✗ ]
 then
