e2e0f303fef61791f1734be16ecc7acde9a2e488
Paul Tarvydas <paul.tarvydas@gmail.com>
2026-02-05 11:30:14 -0500

    save

--- xinterpret.py
+++ xinterpret.py
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-if (found) {
-    if (%incompilingstate) {
-        if (foundimmediate) {
-            exec(item)
-        } else {
-            compileword(item)
-        }
-    } else {
-        exec(item)
-    }
-} else {
-    if (%incompilingstate) {
-        if (%isinteger(item)) {
-            compileinteger(item)
-        } else {
-            if (%isfloat(item)) {
-                compilefloat(item)
-            } else {
-                %returnFalse
-            }
-        }
-    } else {
-        if (%isinteger(item)) {
-            pushasinteger(item)
-        } else {
-            if (%isfloat(item)) {
-                pushasfloat(item)
-            } else {
-                %returnFalse
-            }
-        }
-    }
-}
-=======
 if found_p:
     if incompilingstate_p:
         if foundimmediate_p:
@@ -62,4 +27,3 @@ else:
                 push_as_float(item)
             else:
                 return_False
->>>>>>> v2dtree
